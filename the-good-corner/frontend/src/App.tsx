f8718e246d86c24ff1ab02aefe87f2d36ca447dd
LoicBrassart <loic.brassart@gmail.com>
2024-09-30 12:06:37 +0200

    Add base frontend

---
+++ the-good-corner/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import RecentAds from "./components/RecentAds";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<RecentAds />
+		</>
+	);
+}
+
+export default App;
