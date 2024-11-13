062b7dd3b2f5a43ae129d2b7e04f3257b9b52265
LoicBrassart <loic.brassart@gmail.com>
2024-11-13 17:34:44 +0100

    see readme.md

--- the-good-corner/backend/src/index.ts
+++ the-good-corner/backend/src/index.ts
@@ -1,6 +1,25 @@
 import "reflect-metadata";
 import { dataSource } from "./config/db";
+import { buildSchema } from "type-graphql";
+import { AdResolver } from "./resolvers/AdResolver";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 const port = 3000;
 
-await dataSource.initialize();
+const start = async () => {
+	await dataSource.initialize();
+
+	const schema = await buildSchema({
+		resolvers: [AdResolver],
+	});
+
+	const apiServer = new ApolloServer({ schema });
+
+	const { url } = await startStandaloneServer(apiServer, {
+		listen: { port: port },
+	});
+	console.log("Hey, ça marche ! =D");
+	console.log(url);
+};
+start();
