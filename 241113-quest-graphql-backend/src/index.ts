70855800ed09f331f3ae871a080c81652858f1d2
LoicBrassart <loic.brassart@gmail.com>
2024-11-13 15:59:09 +0100

    Link Book entity to Typeorm/SQLite

--- 241113-quest-graphql-backend/src/index.ts
+++ 241113-quest-graphql-backend/src/index.ts
@@ -2,16 +2,21 @@ import "reflect-metadata";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { Arg, buildSchema, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
+import { BaseEntity, Column, DataSource, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 @ObjectType()
-class Book {
+class Book extends BaseEntity {
 	@Field()
+	@PrimaryGeneratedColumn()
 	id: string;
-
+	
 	@Field()
+	@Column()
 	title: string;
-
+	
 	@Field()
+	@Column()
 	author: string;
 }
 
@@ -19,7 +24,7 @@ class Book {
 class BookInput {
 	@Field()
 	title: string;
-
+	
 	@Field()
 	author: string;
 }
@@ -27,75 +32,38 @@ class BookInput {
 @Resolver(Book)
 class BookResolver {
 	@Query(() => [Book])
-	getBooks() {
+	async getBooks() {
+		const books = await Book.find()
 		return books;
 	}
-
+	
 	@Query(() => Book)
-	getBookById( @Arg("bookId") bookId: string) {
-		return books.find((book) => {
-			return book.id === bookId;
-		});
+	async getBookById( @Arg("bookId") id: string) {
+		const book = await Book.findOneByOrFail({id})
+		return book;
 	}
-
+	
 	@Mutation(() => Book)
-	addBook(@Arg("data") {title, author}: BookInput) {
-		const lastId = Number(books.at(-1).id);
-		const newId = (lastId + 1).toString();
-		books.push({ id: newId, title: title, author: author });
-		return books.at(-1);
+	async addBook(@Arg("data") {title, author}: BookInput) {
+		const book = new Book()
+		book.title=title;
+		book.author=author;
+		await book.save()
+		return book;
 	}
 }
 
-// const myTypes = `#graphql
-//     type Book {
-//         title: String
-//         author: String
-//         id: ID
-//     }
+const dataSource = new DataSource({
+	entities: [Book],
+  
+	type: "sqlite",
+	database:"./database.sqlite",
+  
+	synchronize: true,
+	logging: true,
+  });
 
-//     type Query {
-//         getBooks: [Book]
-//         getBookById(bookId: ID): Book
-//     }
-//     type Mutation {
-//         addBook(title:String, author: String): Book
-//     }
-// `;
-
-const books: Book[] = [
-	{
-		id: "12",
-		title: "The Awakening",
-		author: "Kate Chopin",
-	},
-	{
-		id: "134",
-		title: "City of Glass",
-		author: "Paul Aster",
-	},
-];
-
-// const myResolvers = {
-// 	Query: {
-// 		getBooks: () => {
-// 			return books;
-// 		},
-// 		getBookById: (_, args) => {
-// 			return books.find((book) => {
-// 				return book.id === args.bookId;
-// 			});
-// 		},
-// 	},
-// 	Mutation: {
-// 		addBook: (_, args) => {
-// 			const lastId = Number(books.at(-1).id);
-// 			const newId = (lastId + 1).toString();
-// 			books.push({ id: newId, title: args.title, author: args.author });
-// 			return books.at(-1);
-// 		},
-// 	},
-// };
+dataSource.initialize()
 
 const schema = await buildSchema({
 	resolvers: [BookResolver]
